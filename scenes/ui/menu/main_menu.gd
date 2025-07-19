8382a9a5404b0d3a67694454f651e9e4abf166d9
Jan Hofman <jan.hofman1@tul.cz>
2025-07-20 00:54:38 +0200

    Main menu

---
+++ scenes/ui/menu/main_menu.gd
@@ -0,0 +1,50 @@
+extends Control
+
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/Swamp3.tscn")
+
+
+func _on_play_button_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_play_button_3_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_play_button_4_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_play_button_mouse_entered() -> void:
+	$ButtonContainer/PlayButton/Label.modulate = Color(0.563, 0.223, 0.204)
+
+
+func _on_play_button_mouse_exited() -> void:
+	$ButtonContainer/PlayButton/Label.modulate = Color.WHITE
+
+
+func _on_play_button_2_mouse_entered() -> void:
+	$ButtonContainer/PlayButton2/Label.modulate = Color(0.563, 0.223, 0.204)
+
+
+func _on_play_button_2_mouse_exited() -> void:
+	$ButtonContainer/PlayButton2/Label.modulate = Color.WHITE
+
+
+func _on_play_button_3_mouse_entered() -> void:
+	$ButtonContainer/PlayButton3/Label.modulate = Color(0.563, 0.223, 0.204)
+
+
+func _on_play_button_3_mouse_exited() -> void:
+	$ButtonContainer/PlayButton3/Label.modulate = Color.WHITE
+
+
+func _on_play_button_4_mouse_entered() -> void:
+	$ButtonContainer/PlayButton4/Label.modulate = Color(0.563, 0.223, 0.204)
+
+
+func _on_play_button_4_mouse_exited() -> void:
+	$ButtonContainer/PlayButton4/Label.modulate = Color.WHITE
